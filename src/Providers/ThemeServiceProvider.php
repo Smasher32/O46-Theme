0b296762ca9fce503ebe13dadf6fc6cdfc75d741
Smasher32 <68631806+Smasher32@users.noreply.github.com>
2020-07-22 10:17:50 +0200

    Initial commit

---
+++ src/Providers/ThemeServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+ 
+namespace O46-Theme\Providers;
+ 
+use Plenty\Plugin\ServiceProvider;
+ 
+class O46-ThemeServiceProvider extends ServiceProvider
+{
+ 
+	/**
+	 * Register the service provider.
+	 */
+	public function register()
+	{
+ 
+	}
+}
